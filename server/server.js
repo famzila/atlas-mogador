a29e1fb25de64698a63ab242015074717065835f
famzil <famzil.contact@gmail.com>
2021-05-23 19:02:10 +0200

    Fix pipe

--- server/server.js
+++ server/server.js
@@ -1,67 +1,48 @@
 const express = require('express');
-const compression = require('compression');
-const cors = require('cors');
-const path = require('path');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// const articlesRouter = require('./routes/articles.js');
-// const contactsRouter = require('./routes/contacts.js');
+const path = require('path');
+require('dotenv').config();
 
-// Express port
-const port = process.env.PORT || 5000;
-// const DB_URI =
-//   process.env.REACT_APP_DB_URI || 'mongodb://localhost:27017/frontDB';
-// mongoose.connect(DB_URI, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
 
-// Open connection
-// const connection = mongoose.connection;
-// connection
-//   .once('open', function () {
-//     console.log('[DB]: Connected Successfully');
-//   })
-//   .on('error', function (error) {
-//     console.log('[DB]: Connection Failed: ', error);
-//   });
-// start app
-const app = express();
+const MONGODB_URI = 'mongodb://localhost/atlas-mogador';
+const PORT = process.env.PORT || 5000;
 
-const shouldCompress = (req, res) => {
-  if (req.headers['x-no-compression']) {
-    // Will not compress responses, if this header is present
-    return false;
-  }
-  // Resort to standard compression
-  return compression.filter(req, res);
-};
+mongoose.connect(MONGODB_URI , {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+});
 
-// Compress all HTTP responses
-app.use(
-  compression({
-    // filter: Decide if the answer should be compressed or not,
-    // depending on the 'shouldCompress' function above
-    filter: shouldCompress,
-    // threshold: It is the byte threshold for the response
-    // body size before considering compression, the default is 1 kB
-    threshold: 0,
-  }),
-);
+let app = express();
 
-// CORS
-app.use(cors());
 // Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+
+app.use((req, res, next) => {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+     if (req.method === 'OPTIONS') {
+         res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+         return res.status(200).json({});
+     }
+     next();
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Server started on port', PORT);
 });
